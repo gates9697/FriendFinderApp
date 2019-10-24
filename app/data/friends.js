fc38e95b4040f4e68178c3622edb494fe426f437
Anthony Gates <setag9697@yahoo.com>
2019-10-23 23:09:50 -0500

    first commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,99 +1,33 @@
 var friends = [
     {
       name: "Tanner",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--1",
-      scores: [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
+      photo: "https://twincitiesheadshots.com/wp-content/uploads/2018/03/Galleria-Dude-optimized-for-web.jpg",
+      scores: [ "1","4","5","2","1","5","4","4","1","5"]
     },
     {
       name: "Devin",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--3",
-      scores: [
-        "4",
-        "2",
-        "5",
-        "1",
-        "3",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2"
-      ]
+      photo: "http://www.cityheadshots.com/uploads/5/1/2/1/5121840/mjb-6951_2_orig.jpg",
+      scores: [ "2","3","1","2","2","3","1","5","2","4"]
     },
     {
       name: "Tyler",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--5",
-      scores: [
-        "5",
-        "2",
-        "2",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
-        "5",
-        "5"
-      ]
+      photo: "https://geeks.eku.edu/sites/geeks.eku.edu/files/images/lingjie_cai.jpg",
+      scores: [ "5","5","2","3","1","4","2","2","2","5"]
     },
     {
       name: "Jessy",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--4",
-      scores: [
-        "3",
-        "3",
-        "4",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "2",
-        "3"
-      ]
+      photo: "https://cdn.goodgallery.com/aff0103d-b383-4d77-a4d3-549a4ba909b4/r/1024/2b7bx8v5/business-headshot-photographer-professional-headshots-3.jpg",
+      scores: [ "3","2","2","3","1","2","2","4","3","3"]
     },
     {
       name: "Alexandria",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--7",
-      scores: [
-        "4",
-        "3",
-        "4",
-        "1",
-        "5",
-        "2",
-        "5",
-        "3",
-        "1",
-        "4"
-      ]
+      photo: "http://www.minneapolisheadshots.com/gallery/main/professional-woman1.jpg",
+      scores: [ "4","1","3","5","2","5","1","4","3","4"]
     },
     {
       name: "Malik",
-      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--6",
-      scores: [
-        "4",
-        "4",
-        "2",
-        "3",
-        "2",
-        "2",
-        "3",
-        "2",
-        "4",
-        "5"
-      ]
+      photo: "https://cdn.goodgallery.com/4dcc15ec-978e-4e08-b81c-e0389ca9440d/r/1024/2b8ze69e/professional-headshots-albany.jpg",
+      scores: [ "5","4","2","3","2","2","3","2","4","4"]
     }
   
   ];
