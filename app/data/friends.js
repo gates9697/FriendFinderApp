1f287dcfc779c8bfd308e8b071da531e88da11ad
Anthony Gates <setag9697@yahoo.com>
2019-10-23 19:44:56 -0500

    beginning files

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name: "Tanner",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--1",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Devin",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--3",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Tyler",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--5",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Jessy",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--4",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Alexandria",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--7",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Malik",
+      photo: "https://headshotcrew.com/dahiya/gallery/103333#view--6",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
